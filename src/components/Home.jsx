5175de9eaf864c5586d91c5c13bb66d0cd75ad0d
Hemanga Rana <95037269+Hemanga-Rana@users.noreply.github.com>
2023-10-05 16:39:21 +0530

    do some adjustment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl mt-28 sm:text-7xl font-bold text-white">
+          <h2 className="text-4xl mt-24 sm:text-7xl font-bold text-white">
             I'm a MERN Stack Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
@@ -39,7 +39,7 @@ const Home = () => {
           <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full mb-16"
+            className="rounded-2xl mx-auto w-2/3 md:w-full mb-20"
           />
         </div>
       </div>
